da8b2660dc869495b87a0ec628965452be81056f
dingledow <david@ingledow.co.uk>
2014-11-02 17:42:59 +0000

    first commit

---
+++ Flash/data/speechlines.js
@@ -0,0 +1,44 @@
+{
+  "observations":{
+  
+    "TRASH":{
+      "look": "Yeah, that's the trash.",
+      "use": "I don't think taking out the trash is going to help this situation."
+    },
+    
+    "POLE":{
+      "look": "That pole could come in handy.",
+      "use": "I am getting that pole."
+    },
+    
+    "DEFAULT": "This is my default line for not doing something."
+  },
+
+  "dialog":{
+   /*
+      "CHARACTERNAME":{
+           "talk":[
+              {
+                "option":"This is the first speech option.",
+                "response":[
+                  ["character", "This is the first response."],
+                  ["player", "Now the player says something in return"]
+                ]
+              },
+              {
+                "option":"This is the second speech option.",
+                "response":[
+                  ["character", "This is just a one-line response."],
+                ]
+              },
+           ], 
+          "useObject": {
+            "OBJECTNAME":[
+              ["player","I'm using this object on you and now I'm talking about it."],
+              ["character", "I am responding!"]
+             ]
+          }
+      }
+    */
+  }
+}
